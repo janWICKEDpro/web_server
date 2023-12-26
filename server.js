ef36b8f1545deb9e92f97ca8fc6889a378a32414
janWICKEDpro <janroyal45@gmail.com>
2023-12-26 20:29:30 +0100

    read curl request path  and server html based on path

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const  net= require('net');
+const fileAccess = require('fs');
 
 const PORT = 5000;
 const ADDRESS = '127.0.0.1';
@@ -7,15 +8,22 @@ let server = net.createServer(onClientConnected);
 
 server.listen(PORT);
 
-console.log('listening to connections ');
+console.log('listening to connections');
 
-function onClientConnected(socket){
+async function onClientConnected(socket){
     console.log(`New client: ${socket.remoteAddress}  `);
     
-    socket.on('data', (data) =>{
+    socket.on('data',  (data) =>{
             console.log(data.toString());
           const  requestData = data.toString().split('\n')[0].split(' ')[1];
-          const message = `HTTP/1.1 200 OK\r\n\r\nRequested path: ${requestData}\r\n`
+          
+          
+          let message = '';
+          if(requestData == '/' || '/index.html'){
+        fileAccess.readFile('index.html', 'utf-8', (err, result)=>{
+                if(err){
+                    message = `HTTP/1.1 400 Not Found \r\n`; 
+                      
             var response = [
                 'HTTP/1.1 200 OK',
                 'Content-Type: text/plain',
@@ -24,6 +32,34 @@ function onClientConnected(socket){
                 message
               ].join('\r\n');
             socket.write(response);
+                    return;
+                }
+                message =  `HTTP/1.1 200 OK\r\n\r\n ${result}\r\n`;
+                  
+            var response = [
+                'HTTP/1.1 200 OK',
+                'Content-Type: text/plain',
+               `Content-Length: ${message.length}`,
+                '',
+                message
+              ].join('\r\n');
+            socket.write(response);
+                return result;
+            });
+            
+          }else{
+            message = `HTTP/1.1 400 Not Found \r\n`; 
+              
+            var response = [
+                'HTTP/1.1 200 OK',
+                'Content-Type: text/plain',
+               `Content-Length: ${message.length}`,
+                '',
+                message
+              ].join('\r\n');
+            socket.write(response);
+          }
+        
     });
 
    
